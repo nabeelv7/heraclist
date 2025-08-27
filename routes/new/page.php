499a6636b54da832ab20860b5690e3988e65ed03
nabeelv7 <nabeelsadiq.dev@gmail.com>
2025-08-27 16:38:58 +0500

    update: added hx-boost to /new form

--- routes/new/page.php
+++ routes/new/page.php
@@ -1,7 +1,7 @@
 <main class="flex flex-col gap-7 justify-center items-center min-h-screen">
     <h1 class="text-4xl font-bold">📝 Create new todo</h1>
 
-    <form action="/create" class="flex gap-2" method="POST">
+    <form hx-boost="true" action="/create" class="flex gap-2" method="POST">
         <input type="text" name="todo" class="input" placeholder="Enter task here..">
         <button type="submit" class="btn btn-accent">Create</button>
     </form>
